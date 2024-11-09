3dec00abd0d1f2e202e9d27746ac0008b49668a5
hugocollin <hugo.collin3@gmail.com>
2024-11-09 13:55:15 +0100

    Corrections micro bugs

--- api.py
+++ api.py
@@ -5,6 +5,8 @@ class API:
     # Méthode pour charger une page de données
     def get_data_page(self, url):
         response = requests.get(url)
+
+        # Vérification du statut de la réponse
         if response.status_code == 200:
             return response.json()
         else:
@@ -37,32 +39,25 @@ class API:
                 print(f"Erreur lors du traitement : {str(e)}")
                 break
 
-        # Transformation des données en DataFrame avec types optimisés
-        all_dpe = pd.DataFrame(all_dpe, dtype='str')
+        # Transformation des données en DataFrame
+        all_dpe = pd.DataFrame(all_dpe)
 
         # Mise en forme des données
-        all_dpe.drop(columns=["_score"], inplace=True, errors='ignore')
+        all_dpe = all_dpe.drop(columns=["_score"])
         all_dpe["Date_réception_DPE"] = all_dpe["Date_réception_DPE"].str.replace("-", "")
 
         # Si des données sont déjà présentes
         try:
-            df = pd.read_csv(
-                "assets/data_69.csv",
-                sep="|",
-                dtype={"Date réception DPE": str},
-                low_memory=False
-            )
-            
-            # Conversion en datetime pour comparer les dates
-            df["Date réception DPE"] = pd.to_datetime(df["Date réception DPE"], format="%Y%m%d", errors='coerce')
-            all_dpe["Date_réception_DPE"] = pd.to_datetime(all_dpe["Date_réception_DPE"], format="%Y%m%d", errors='coerce')
+            all_data = []
+            df = pd.read_csv("assets/data_69.csv", sep="|")
             
             # Récupération de la date des DPE les plus récents
             max_date = df["Date réception DPE"].max()
             
             # Filtrage des nouveaux DPE
-            all_dpe = all_dpe[all_dpe["Date_réception_DPE"] > max_date]
+            all_dpe = all_dpe[all_dpe["Date_réception_DPE"] > str(max_date)]
             
+            # Si aucun nouveau DPE n'est trouvé, arrêt de la fonction
             if all_dpe.empty:
                 print("Aucun nouveau DPE trouvé")
                 return
@@ -73,56 +68,50 @@ class API:
             dpe_list = all_dpe["N°DPE"].tolist()
             
             # Division de la liste en chunks de 500
-            for i in range(0, len(dpe_list), 500):
-                chunk = dpe_list[i:i + 500]
+            for i in range(0, len(dpe_list), 100):
+                chunk = dpe_list[i:i + 100]
                 dpe_str = '%2C'.join(map(str, chunk))
-                try:
+                try :
                     data = self.get_data_page(f"https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-existants/lines?size=10000&q={dpe_str}&q_fields=N%C2%B0DPE&select=P%C3%A9riode_construction%2CSurface_habitable_logement%2CNombre_niveau_logement%2CType_b%C3%A2timent%2CHauteur_sous-plafond%2CType_%C3%A9nergie_principale_chauffage%2CType_%C3%A9nergie_principale_ECS%2CConso_5_usages_%C3%A9_finale%2CConso_chauffage_%C3%A9_finale%2CConso_ECS_%C3%A9_finale%2CClasse_altitude%2CEtiquette_DPE%2CNom__commune_(BAN)%2CCode_postal_(BAN)%2CDate_r%C3%A9ception_DPE%2C_geopoint")
                     
-                    # Transformation immédiate et écriture dans le CSV
-                    chunk_data = pd.DataFrame(data["results"], dtype='str')
-                    chunk_data.drop(columns=["_score"], inplace=True, errors='ignore')
-                    chunk_data[["Latitude", "Longitude"]] = chunk_data["_geopoint"].str.split(",", expand=True)
-                    chunk_data.drop(columns=["_geopoint"], inplace=True, errors='ignore')
-                    chunk_data["Date_réception_DPE"] = chunk_data["Date_réception_DPE"].str.replace("-", "")
-                    chunk_data["Date_réception_DPE_graph"] = chunk_data["Date_réception_DPE"].str[:6]
-                    chunk_data.rename(columns={
-                        "Conso_5_usages_é_finale": "Consommation totale",
-                        "Nom__commune_(BAN)": "Nom commune",
-                        "Date_réception_DPE": "Date réception DPE",
-                        "Conso_ECS_é_finale": "Consommation ECS",
-                        "Code_postal_(BAN)": "Code postal",
-                        "Hauteur_sous-plafond": "Hauteur sous-plafond",
-                        "Surface_habitable_logement": "Surface habitable logement",
-                        "Nombre_niveau_logement": "Nombre niveau logement",
-                        "Période_construction": "Période construction",
-                        "Conso_chauffage_é_finale": "Consommation chauffage",
-                        "Type_bâtiment": "Type bâtiment",
-                        "Classe_altitude": "Classe altitude",
-                        "Type_énergie_principale_ECS": "Type énergie ECS",
-                        "Type_énergie_principale_chauffage": "Type énergie chauffage",
-                        "Etiquette_DPE": "Étiquette DPE"
-                    }, inplace=True)
-                    
-                    # Ajout des données au CSV
-                    chunk_data.to_csv(
-                        "assets/data_69.csv",
-                        index=False,
-                        sep="|",
-                        encoding="utf-8",
-                        mode="a",
-                        header=False
-                    )
-                    
-                    print(f"Chunk {i//500 + 1} traité et ajouté au fichier.")
+                    # Ajout des données
+                    all_data.extend(data["results"])
                 except Exception as e:
-                    print(f"Erreur lors du traitement du chunk {i//500 + 1} : {e}")
-                    break
+                    print(f"Erreur lors du traitement : {str(e)}")
+                    break  
+
+            # Transformation des données en DataFrame
+            all_data = pd.DataFrame(all_data)
+
+            # Mise en forme des données
+            all_data = all_data.drop(columns=["_score"])
+            all_data[["Latitude", "Longitude"]] = all_data["_geopoint"].str.split(",", expand=True)
+            all_data = all_data.drop(columns=["_geopoint"])
+            all_data["Date_réception_DPE"] = all_data["Date_réception_DPE"].str.replace("-", "")
+            all_data["Date_réception_DPE_graph"] = all_data["Date_réception_DPE"].str[:6]
+            all_data = all_data.rename(columns={
+                "Conso_5_usages_é_finale": "Consommation totale",
+                "Nom__commune_(BAN)": "Nom commune",
+                "Date_réception_DPE": "Date réception DPE",
+                "Conso_ECS_é_finale": "Consommation ECS",
+                "Code_postal_(BAN)": "Code postal",
+                "Hauteur_sous-plafond": "Hauteur sous-plafond",
+                "Surface_habitable_logement": "Surface habitable logement",
+                "Nombre_niveau_logement": "Nombre niveau logement",
+                "Période_construction": "Période construction",
+                "Conso_chauffage_é_finale": "Consommation chauffage",
+                "Type_bâtiment": "Type bâtiment",
+                "Classe_altitude": "Classe altitude",
+                "Type_énergie_principale_ECS": "Type énergie ECS",
+                "Type_énergie_principale_chauffage": "Type énergie chauffage",
+                "Etiquette_DPE": "Étiquette DPE"
+                })
+            
+            # Ajout des nouvelles données au fichier data_69.csv
+            all_data.to_csv("assets/data_69.csv", index=False, sep="|", encoding="utf-8", mode="a", header=False)
 
             print(f"{len(dpe_list)} DPE ajoutés")
             
-        except pd.errors.DtypeWarning:
-            print("Avertissement de type de données lors de la lecture du CSV.")
         except FileNotFoundError:
             # Récupération des données complètes si le fichier n'existe pas
             while True:
@@ -130,46 +119,12 @@ class API:
                     if page_number == 1:
                         data = self.get_data_page("https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-existants/lines?size=10000&q=69&q_fields=N%C2%B0_d%C3%A9partement_%28BAN%29&select=P%C3%A9riode_construction%2CSurface_habitable_logement%2CNombre_niveau_logement%2CType_b%C3%A2timent%2CHauteur_sous-plafond%2CType_%C3%A9nergie_principale_chauffage%2CType_%C3%A9nergie_principale_ECS%2CConso_5_usages_%C3%A9_finale%2CConso_chauffage_%C3%A9_finale%2CConso_ECS_%C3%A9_finale%2CClasse_altitude%2CEtiquette_DPE%2CNom__commune_(BAN)%2CCode_postal_(BAN)%2CDate_r%C3%A9ception_DPE%2C_geopoint")
                         total_rows = data.get("total")
-                        print(f"{total_rows} nouveaux DPE trouvés : récupération des données en cours, cela peut prendre un moment...")
+                        print(f"{len(total_rows)} nouveaux DPE trouvés : récupération des données en cours, cela peut prendre un moment...")
                     else:
                         data = self.get_data_page(url)
 
-                    # Transformation immédiate et écriture dans le CSV
-                    chunk_data = pd.DataFrame(data["results"], dtype='str')
-                    chunk_data.drop(columns=["_score"], inplace=True, errors='ignore')
-                    chunk_data[["Latitude", "Longitude"]] = chunk_data["_geopoint"].str.split(",", expand=True)
-                    chunk_data.drop(columns=["_geopoint"], inplace=True, errors='ignore')
-                    chunk_data["Date_réception_DPE"] = chunk_data["Date_réception_DPE"].str.replace("-", "")
-                    chunk_data["Date_réception_DPE_graph"] = chunk_data["Date_réception_DPE"].str[:6]
-                    chunk_data.rename(columns={
-                        "Conso_5_usages_é_finale": "Consommation totale",
-                        "Nom__commune_(BAN)": "Nom commune",
-                        "Date_réception_DPE": "Date réception DPE",
-                        "Conso_ECS_é_finale": "Consommation ECS",
-                        "Code_postal_(BAN)": "Code postal",
-                        "Hauteur_sous-plafond": "Hauteur sous-plafond",
-                        "Surface_habitable_logement": "Surface habitable logement",
-                        "Nombre_niveau_logement": "Nombre niveau logement",
-                        "Période_construction": "Période construction",
-                        "Conso_chauffage_é_finale": "Consommation chauffage",
-                        "Type_bâtiment": "Type bâtiment",
-                        "Classe_altitude": "Classe altitude",
-                        "Type_énergie_principale_ECS": "Type énergie ECS",
-                        "Type_énergie_principale_chauffage": "Type énergie chauffage",
-                        "Etiquette_DPE": "Étiquette DPE"
-                    }, inplace=True)
-                    
-                    # Ajout des données au CSV
-                    chunk_data.to_csv(
-                        "assets/data_69.csv",
-                        index=False,
-                        sep="|",
-                        encoding="utf-8",
-                        mode="a",
-                        header=False
-                    )
-                    
-                    print(f"Chunk {page_number} traité et ajouté au fichier.")
+                    # Ajout des données
+                    all_data.extend(data["results"])
                     
                     # Récupération de l'URL de la page suivante
                     url = data.get("next")
@@ -178,8 +133,38 @@ class API:
 
                     page_number += 1
                 except Exception as e:
-                    print(f"Erreur lors du traitement de la page {page_number} : {e}")
+                    print(f"Erreur lors du traitement : {str(e)}")
                     break
+
+            # Transformation des données en DataFrame
+            all_data = pd.DataFrame(all_data)
+
+            # Mise en forme des données
+            all_data = all_data.drop(columns=["_score"])
+            all_data[["Latitude", "Longitude"]] = all_data["_geopoint"].str.split(",", expand=True)
+            all_data = all_data.drop(columns=["_geopoint"])
+            all_data["Date_réception_DPE"] = all_data["Date_réception_DPE"].str.replace("-", "")
+            all_data["Date_réception_DPE_graph"] = all_data["Date_réception_DPE"].str[:6]
+            all_data = all_data.rename(columns={
+                "Conso_5_usages_é_finale": "Consommation totale",
+                "Nom__commune_(BAN)": "Nom commune",
+                "Date_réception_DPE": "Date réception DPE",
+                "Conso_ECS_é_finale": "Consommation ECS",
+                "Code_postal_(BAN)": "Code postal",
+                "Hauteur_sous-plafond": "Hauteur sous-plafond",
+                "Surface_habitable_logement": "Surface habitable logement",
+                "Nombre_niveau_logement": "Nombre niveau logement",
+                "Période_construction": "Période construction",
+                "Conso_chauffage_é_finale": "Consommation chauffage",
+                "Type_bâtiment": "Type bâtiment",
+                "Classe_altitude": "Classe altitude",
+                "Type_énergie_principale_ECS": "Type énergie ECS",
+                "Type_énergie_principale_chauffage": "Type énergie chauffage",
+                "Etiquette_DPE": "Étiquette DPE"
+                })
+            
+            # Sauvegarde des données en CSV
+            all_data.to_csv("assets/data_69.csv", index=False, sep="|", encoding="utf-8")
 
             print(f"{total_rows} DPE ajoutés")
     
